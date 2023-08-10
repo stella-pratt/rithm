974c6412b1da3ac5c701e88ccb06588f3f75db85
Sebastian Pratt <sebastian.pratt@student.onslow.school.nz>
2023-08-11 00:13:29 +1200

    made frame for ticket sales

--- js/ticket.js
+++ js/ticket.js
@@ -15,3 +15,22 @@ document.addEventListener("mouseleave", function () {
 document.addEventListener("mouseenter", function () {
     mouse_window("enter");
 })
+
+
+const space = document.querySelectorAll(".space")
+const space_section = []
+space.forEach((spacey, index) => {
+    space_section.push([])
+    // fill the grid with circles
+    for (let r = 0; r < 12; r++) { // repeat for 14 rows
+        for (let c = 0; c < 17; c++) { // repeat for 20 columns
+            let seat = document.createElement("div");
+            seat.classList.add("seat");
+            seat.style.gridRow = (r + 1).toString();
+            seat.style.gridColumn = (c + 1).toString();
+            console.log(seat.style.gridRow)
+            console.log(seat.style.gridColumn)
+            spacey.appendChild(seat);
+        }
+    }
+})
