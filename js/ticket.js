b5eb07c5b137a03c1b25e721c16fa3880458ef37
Sebastian Pratt <sebastian.pratt@student.onslow.school.nz>
2023-08-12 00:25:22 +1200

    zoom and circles appear

--- js/ticket.js
+++ js/ticket.js
@@ -4,17 +4,48 @@ document.addEventListener("mousemove", function(e) {
     mouse_move_updates(e)
 });
 
-// add a click checker to add buttons to elements
-document.addEventListener("click", function(e) {
-    menu_btns(e)
-})
-
 document.addEventListener("mouseleave", function () {
     mouse_window("leave");
 })
 document.addEventListener("mouseenter", function () {
     mouse_window("enter");
 })
+
+// center the stage container using top and left
+let stage_container = document.querySelector(".stage_container");
+let parent_stage = document.querySelector(".right_side");
+// save for reuse later
+let default_stage_top = (parent_stage.getBoundingClientRect()["height"] - stage_container.getBoundingClientRect()["height"])/2 + "px";
+let default_stage_left = (parent_stage.getBoundingClientRect()["width"] - stage_container.getBoundingClientRect()["width"])/2 + "px";
+stage_container.style.top = default_stage_top;
+stage_container.style.left = default_stage_left;
+
+// add a click checker to add buttons to elements
+document.addEventListener("click", function(e) {
+    menu_btns(e) // check if menu buttons clicked
+    if (e.target.classList.contains("space")){
+        // when stage area clicked
+        let section = e.target;
+        const seats = section.querySelectorAll(".seat");
+        // zoom in and fade section
+        stage_container.style.width = "175%";
+        section.style.background = "var(--bg_black)";
+        // show seats
+        seats.forEach((seat) => {seat.classList.add("seat_show");})
+        // reset the centering of the stage container
+        stage_container.style.top = "0";
+        stage_container.style.left = "0";
+        // get distance from section to edge of .right_side
+        let section_top = (parent_stage.getBoundingClientRect()["height"] - section.getBoundingClientRect()["height"])/2;
+        let section_left = (parent_stage.getBoundingClientRect()["width"] - section.getBoundingClientRect()["width"])/2;
+        // get distances to edge of stage container
+        let stage_top = section.getBoundingClientRect()["top"]-stage_container.getBoundingClientRect()["top"];
+        let stage_left = section.getBoundingClientRect()["left"]-stage_container.getBoundingClientRect()["left"];
+        // transform the stage container to the new centered position
+        stage_container.style.transform = `translate(${section_left - stage_left + "px"}, ${section_top - stage_top + "px"})`;
+    }
+})
+
 
 
 const space = document.querySelectorAll(".space")
@@ -28,9 +59,8 @@ space.forEach((spacey, index) => {
             seat.classList.add("seat");
             seat.style.gridRow = (r + 1).toString();
             seat.style.gridColumn = (c + 1).toString();
-            console.log(seat.style.gridRow)
-            console.log(seat.style.gridColumn)
             spacey.appendChild(seat);
         }
     }
-})
+});
+
