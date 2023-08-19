e6b46b321f5d7903f1e09298ee233d342430e799
Sebastian Pratt <sebastian.pratt@student.onslow.school.nz>
2023-08-19 23:58:34 +1200

    hover seat working

--- js/ticket.js
+++ js/ticket.js
@@ -1,10 +1,22 @@
 import {menu_btns, mouse_move_updates, mouse_window} from "./global_functions.js";
-let seat_hover = document.querySelector(".seat_hover")
+
+let seat_hover = document.querySelector(".seat_hover");
 let cursor = document.querySelector(".mouse_follow");
 let SPACE_ROWS = 12;
+let PRICES = {"1": 100, "2": 100, "3": 100, "4": 90, "5": 90, "6": 90, "7": 80, "8": 80, "9": 80};
 
 // define row names
 const rows = ['just to make "A" have an index of 1', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ']
+
+
+function mySplice(num_remove, array_remove) {
+    // remove the elements from the array if they are in it
+    if (array_remove.includes(num_remove)){
+        array_remove.splice(array_remove.indexOf(num_remove), 1);
+    }
+    return array_remove;
+}
+
 
 document.addEventListener("mousemove", function(e) {
     mouse_move_updates(e)
@@ -19,12 +31,53 @@ document.addEventListener("mousemove", function(e) {
     if (document.querySelector(".seat:hover") != null){
         document.querySelector(".seat_hover").classList.add("seatpop_show");
         // get row
-        let sect_row_num = (e.target.style.gridRow.replace(/\D/g, ''));
-        // add any sections infront
-        let row_num = parseInt(sect_row_num) + (e.target.parentElement.classList[2].replace(/\D/g, '')-1)*SPACE_ROWS;
-        let row = rows[row_num];
-        let seat = e.target.style.gridColumn.replace(/\D/g, '')
-        console.log(row)
+        let row_num = (e.target.style.gridRow.replace(/\D/g, ''));
+        //change the elements to current hover
+        seat_hover.children[0].children[1].innerHTML = e.target.style.gridColumn.replace(/\D/g, '')
+        seat_hover.children[1].children[1].innerHTML = rows[row_num];
+        let seat_section = e.target.parentElement.classList;
+        if (seat_section.contains("space")){
+            seat_hover.children[3].children[1].innerHTML = "Accessible";
+        } else if (seat_section.contains("standard")){
+            seat_hover.children[3].children[1].innerHTML = "Standard";
+        }
+        let sections = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
+        // keep 3 sections based on sect
+        if (seat_section.contains("sect1")){
+            sections = sections.splice(0,3);
+        } else if (seat_section.contains("sect2")){
+            sections = sections.splice(3,3);
+        } else if (seat_section.contains("sect3")){
+            sections = sections.splice(6,3);
+        }
+        // remove the sections that seat cannot be in
+        if (seat_section.contains("standard")){
+            sections = mySplice("1", sections);
+            sections = mySplice("3", sections);
+            sections = mySplice("4", sections);
+            sections = mySplice("6", sections);
+            sections = mySplice("7", sections);
+            sections = mySplice("9", sections);
+        } else {
+            // if accessible seat
+            sections = mySplice("2", sections);
+            sections = mySplice("5", sections);
+            sections = mySplice("8", sections);
+            if (seat_section.contains("left_space")){
+                // remove right sections
+                sections = mySplice("3", sections);
+                sections = mySplice("6", sections);
+                sections = mySplice("9", sections);
+            } else if (seat_section.contains("right_space")){
+                // remove left sections
+                sections = mySplice("1", sections);
+                sections = mySplice("4", sections);
+                sections = mySplice("7", sections);
+            }
+        }
+        seat_hover.children[2].children[1].innerHTML = sections[0];
+        seat_hover.children[4].children[1].innerHTML = "$" + PRICES[sections[0]];
+
     } else {
          document.querySelector(".seat_hover").classList.remove("seatpop_show");
     }
