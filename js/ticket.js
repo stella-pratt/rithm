d9902f79b02ad4e79907e34565fd05716fbaffce
stella.pratt <spratt914@gmail.com>
2023-09-22 14:52:32 +1200

    testing cloud var

--- js/ticket.js
+++ js/ticket.js
@@ -4,6 +4,7 @@ let seat_hover = document.querySelector(".seat_hover");
 let cursor = document.querySelector(".mouse_follow");
 let SPACE_ROWS = 12;
 let PRICES = {"1": 90, "2": 90, "3": 90, "4": 80, "5": 80, "6": 80, "7": 70, "8": 70, "9": 70};
+// cloud testing
 var cloud_test;
 cloud_test++;
 console.log(cloud_test);
